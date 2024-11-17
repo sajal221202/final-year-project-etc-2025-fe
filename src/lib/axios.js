6bd6de9f17d81a9cf7dd1fa4884fa6f87968c7a3
Sajal Gupta <21bei051@ietdavv.edu.in>
2024-11-17 21:50:06 +0530

    url fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1",
+	baseURL: import.meta.env.VITE_MODE === "development" ? "http://localhost:5000/api/v1" : `${import.meta.env.VITE_SERVER}/api/v1`,
 	withCredentials: true,
 });
